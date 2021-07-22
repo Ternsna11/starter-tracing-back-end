59415f6fb1f153a5411df299c31c0651287ae601
Nick Terns <ternsna11@gmail.com>
2021-07-22 14:18:04 -0500

    changed log level in env

--- src/articles/articles.controller.js
+++ src/articles/articles.controller.js
@@ -1,5 +1,6 @@
 const service = require("./articles.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const { rawListeners } = require("../db/connection");
 
 function hasData(req, res, next) {
   if (req.body.data) {
@@ -29,12 +30,16 @@ async function create(req, res) {
     data: newObservation,
   });
 }
+//In the above code, you added two log statements. The first calls debug(), and the second calls trace(). This means that nothing new will be logged with the standard log level of info
 
 async function list(req, res) {
+  const methodName = "list";
+  req.log.debug({ __filename, methodName })
   const data = await service.list();
   res.json({
     data,
   });
+  req.log.trace({ __filename, methodName, return: true, data })
 }
 
 module.exports = {
