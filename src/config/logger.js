6b61c0bea5d4bd7327c30ca02821b2e82fb5c794
Nick Terns <ternsna11@gmail.com>
2021-07-22 14:08:29 -0500

    pretty print

--- src/config/logger.js
+++ src/config/logger.js
@@ -2,13 +2,16 @@ const pinoHttp = require("pino-http");
 const { nanoid } = require("nanoid");
 
 const level = process.env.LOG_LEVEL || "info";
-
+const nodeEnv = process.env.NODE_ENV || "development";
+const prettyPrint = nodeEnv === "development";
 const logger = pinoHttp({
   genReqId: (request) => request.headers["x-request-id"] || nanoid(),
+  level,
+  prettyPrint,
 });
 
 module.exports = logger;
-// this code is to allow us to configure the logger 
+// this code is to allow us to configure the logger
 //In the above code, if the request has an X-Request-ID header, the value of that header will be used as the request ID. Otherwise, nanoid() is called to generate a new ID
 
 // Node converts all header keys to lowercase, so it does not matter what case is used in the original request. You will access the header using all lowercase characters in the key.
